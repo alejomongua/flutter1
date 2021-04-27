e032822ed4ac846e5e18e07ad8a21f8f20ee5ca0
Alejandro Mongua <alejandro.mongua@gmail.com>
2021-04-27 17:54:47 -0500

    se sacan las funciones a metodos de instancia

--- lib/src/pages/contador_page.dart
+++ lib/src/pages/contador_page.dart
@@ -10,32 +10,34 @@ class _ContadorPageState extends State<ContadorPage> {
 
   int _conteo = 0;
 
+  void reset() {
+    _conteo = 0;
+    setState(() {});
+  }
+
+  void sumar() {
+    _conteo++;
+    setState(() {});
+  }
+
+  void restar() {
+    if (_conteo > 0) {
+      _conteo--;
+      setState(() {});
+    }
+  }
+
   Widget _crearBotones() {
     return Row(
       children: [
         SizedBox(width: 30.0),
-        FloatingActionButton(
-            onPressed: () {
-              _conteo = 0;
-              setState(() {});
-            },
-            child: Icon(Icons.restore)),
+        FloatingActionButton(onPressed: reset, child: Icon(Icons.restore)),
         Expanded(child: SizedBox()),
         FloatingActionButton(
-            onPressed: () {
-              _conteo++;
-              setState(() {});
-            },
-            child: Icon(Icons.arrow_upward_sharp)),
+            onPressed: sumar, child: Icon(Icons.arrow_upward_sharp)),
         SizedBox(width: 10.0),
         FloatingActionButton(
-            onPressed: () {
-              if (_conteo > 0) {
-                _conteo--;
-                setState(() {});
-              }
-            },
-            child: Icon(Icons.arrow_downward_sharp))
+            onPressed: restar, child: Icon(Icons.arrow_downward_sharp))
       ],
       mainAxisAlignment: MainAxisAlignment.end,
     );
